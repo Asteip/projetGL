fe38295b8dbe345017e7e3daddd33be7bf094145
Sacha Loriot <madersach@MacBook-Pro-de-Sacha.local>
2016-12-15 23:32:10 +0100

    Ajout des contructeurs + acesseurs pour classes du package dpt

--- gestion-services/src/main/java/com/alma/departements/Module.java
+++ gestion-services/src/main/java/com/alma/departements/Module.java
@@ -2,5 +2,13 @@ package com.alma.departements;
 
 public class Module{
 	
+	private Parcours parcours;
 	
+	public Module(Parcours parcours) {
+		this.parcours = parcours;
+	}
+	
+	public Parcours getParcours() {
+		return this.parcours;
+	}
 }
--- gestion-services/src/main/java/com/alma/departements/Parcours.java
+++ gestion-services/src/main/java/com/alma/departements/Parcours.java
@@ -1,5 +1,36 @@
 package com.alma.departements;
 
+import java.util.ArrayList;
+
 public class Parcours{
+
+	private Departement departement;
+	private ArrayList<Module> modules;
+	private String nom;
 	
+	public Parcours(Departement departement, String nom) {
+		this.departement = departement;
+		this.modules = new ArrayList<Module>();
+		this.nom = nom;
+	}
+	
+	public Departement getDepartement() {
+		return this.departement;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public ArrayList<Module> getModules() {
+		return this.modules;
+	}
+	
+	public void addModule(Module module) {
+		modules.add(module);
+	}
+	
+	public void deleteModule(Module module) {
+		this.modules.remove(module);
+	}
 }
