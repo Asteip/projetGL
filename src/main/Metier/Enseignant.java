6d374fbf7e2cdd6fa29eb8de20fdea8d2087dcbd
Sacha Loriot <madersach@MacBook-Pro-de-Sacha.local>
2016-12-15 17:08:48 +0100

    Creation package Metier + class Enseignant

---
+++ src/main/Metier/Enseignant.java
@@ -0,0 +1,23 @@
+package Metier;
+public class Enseignant {
+
+	private String nom;
+	private String prenom;
+	private String status;
+	private List<Demande> souhaits;
+	private List<Service> services;
+	private Departement departement;
+	private Contrat contrat;
+	private long id;
+
+	public void creerVoeu(){}
+
+	public void creerDemandeExterieur(){}
+
+	public void creerDemandeSpeciale(){}
+
+	public void consulterSoouhait(){}
+
+	public void getEnseignements(){}
+
+}
