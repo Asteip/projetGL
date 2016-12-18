d25625d69e7a290edc388f6956d811d0df986f47
Sacha Loriot <madersach@MacBook-Pro-de-Sacha.local>
2016-12-18 17:38:11 +0100

    affecterDemande() -> creation in Departement

--- gestion-services/src/main/java/com/alma/departements/Departement.java
+++ gestion-services/src/main/java/com/alma/departements/Departement.java
@@ -1,7 +1,9 @@
 package com.alma.departements;
 
 import com.alma.enseignants.*;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Departement{
 	private String nom;
@@ -22,8 +24,18 @@ public class Departement{
 		
 	}
 	
-	public void affecterEnseignements(Demande demande){
+	public void affecterDemande(Demande demande){
+		Intervention i;
 		
+		if (demande instanceof Voeu) {
+			i = new InterventionDep(demande.getHeures(), new Service(demande.getHeures(), Calendar.getInstance().get(Calendar.YEAR)));
+		}
+		else if (demande instanceof DemandeSpeciale) {
+			i = new CasSpecial(demande.getHeures(), new Service(demande.getHeures(), Calendar.getInstance().get(Calendar.YEAR)));
+		}
+		else if (demande instanceof DemandeInterExt) {
+			i = new InterventionExt(demande.getHeures(), new Service(demande.getHeures(), Calendar.getInstance().get(Calendar.YEAR)));
+		}
 	}
 	
 	public void ImposeInterventionDepartement(Enseignant ens, Enseignement e){
